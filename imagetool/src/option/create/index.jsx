30547eadfa7708c14738360100f0c1145958f6bf
熊诗远 <shiyuan.xiong@qunar.com>
2022-07-05 21:43:03 +0800

    添加文案

--- imagetool/src/option/create/index.jsx
+++ imagetool/src/option/create/index.jsx
@@ -4,17 +4,41 @@ import { HexColorPicker } from 'react-colorful'
 
 const FormItem = Form.Item
 
+function getTextColor(bgColor) {
+  if(parseInt(bgColor.slice(1), 16) > 2050000) {
+    return '#000000'
+  } else {
+    return '#ffffff'
+  }
+}
+
 function getImage (config) {
   console.log('test:', config)
-  const { width, height, bg } = config;
+  const { width: w, height: h, bg, text } = config;
+  const width = typeof w === 'string' ? parseInt(w) : w;
+  const height = typeof h === 'string' ? parseInt(h) : h;
   const canvas = document.createElement('canvas');
-  canvas.width = typeof width === 'string' ? parseInt(width) : width;
-  canvas.height = typeof height === 'string' ? parseInt(height) : height;
+  canvas.width = width;
+  canvas.height = height;
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = bg;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  const canvasUrl = canvas.toDataURL("image/png");
-  return canvasUrl
+  ctx.fillRect(0, 0, width, height);
+  if(text) {
+     ctx.fillStyle = getTextColor(bg);
+     ctx.font = `${Math.round(Math.min(height / 3, width / 3))}px serif`;
+     const measure = ctx.measureText(text);
+     const {actualBoundingBoxAscent, actualBoundingBoxDescent, actualBoundingBoxLeft, actualBoundingBoxRight} = measure
+     const textBoxWidth = actualBoundingBoxRight - actualBoundingBoxLeft;
+     const textBoxHeight = actualBoundingBoxAscent - actualBoundingBoxDescent;
+     ctx.fillText(text, width / 2 - textBoxWidth / 2 , height / 2 + textBoxHeight / 2);
+  }
+  // const canvasUrl = canvas.toDataURL("image/png");
+  return new Promise((resolve, reject) => {
+    canvas.toBlob((file) => {
+      resolve(URL.createObjectURL(file))
+    }, 'image/png')
+  })
+  // return canvasUrl
 }
 
 function getTime() {
@@ -28,38 +52,36 @@ const CreateImage = () => {
   const [url, setUrl] = React.useState('')
   const [time, setTime] = React.useState('')
 
-  const handleCreate = () => {
+  const handleCreate = async() => {
     const value = form.getFieldsValue();
-    const url = getImage({...value, bg})
-    console.log('test:', url)
+    const url = await getImage({...value, bg})
     setUrl(url)
     setTime(getTime())
   }
-  // const download = () => {
-  //   const link = document.createElement('a');
-  //   a.setAttribute('download', 'imageTool.png');
-  //   a.setAttribute('href')
-  // }
+
   return (
-    <div>
+    <div style={{minHeight: '400px'}}>
       <Card title="图片参数">
-        <Form form={form} initialValues={{height: 400, width: 375}} layout="inline" labelCol={{span: 16}}>
+        <Form form={form} initialValues={{height: 400, width: 375, text: 'image'}} layout="inline">
           <FormItem field="height" label="高度">
             <InputNumber suffix="像素" />
           </FormItem> 
           <FormItem field="width" label="宽度">
             <InputNumber suffix="像素" />
           </FormItem>
-          <FormItem label="背景色">
+          <FormItem label="背景">
             <div style={{display: 'flex'}}>
               <Input value={bg} disabled />
-              <div style={{position: 'relative'}} className="color-pick-container">
+              <div style={{position: 'relative', marginLeft: '6px'}} className="color-pick-container">
                 <Button>
                   颜色选择器
                 </Button>
                 <HexColorPicker style={{position: 'absolute', top: 0, left: '100%'}} value={bg} onChange={setbg} className="color-picker"  />
               </div>
             </div>
+          </FormItem>
+          <FormItem label="文案" field="text">
+            <Input  />
           </FormItem>
         </Form>
         <Button type="primary" onClick={handleCreate}>创建</Button>
@@ -81,7 +103,7 @@ const CreateImage = () => {
         url && (
           <Card>
             <div>
-              <small>点击图片下载</small>
+              <small>点击图片下载到本地</small>
             </div>
             <a href={url.replace("image/png", "image/octet-stream")} download={`${time}.png`}>
               <img src ={url} />
